2c180662975d999dca16733be365ecebca5c7314
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-01-31 11:36:07 +0700

    merubah warna background navbar ketika di scroll

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -2,15 +2,32 @@ import {Container, Navbar, Nav} from "react-bootstrap";
 
 import {navLinks} from "../data/index.js";
 import {NavLink} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 const NavbarComponent = () => {
+    const [changeColor, setChangeColor] = useState(false)
+
+    const changeBackgroundColor = () => {
+        if (window.scrollY > 10) {
+            setChangeColor(true)
+        } else {
+            setChangeColor(false)
+        }
+    }
+
+    useEffect(() => {
+        changeBackgroundColor()
+
+        window.addEventListener('scroll', changeBackgroundColor)
+    }, []);
+
     return (
-        <Navbar expand="lg">
+        <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
             <Container>
                 <Navbar.Brand href="#home" className="fs-3 fw-bold">Ngoding</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mx-auto text-center">
+                    <Nav className="mx-auto text-center ">
                         {navLinks.map((navLink) => (
                             <div className="nav-link" key={navLink.id}>
                                 <NavLink to={navLink.path}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ const HomePage = () => {
     return (
         <div className="homepage">
             <header className="w-100 min-vh-100"></header>
+            <div className="kelas w-100 min-vh-100"></div>
         </div>
     );
 };
