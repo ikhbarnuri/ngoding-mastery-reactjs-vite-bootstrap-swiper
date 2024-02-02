eaf3120a135d81968083c7cf50f9bfa512be4f40
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-02-02 22:24:31 +0700

    menambahkan ScrollToTop

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./dist/css/main.css"
 
 import {BrowserRouter} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
+            <ScrollToTop/>
             <App/>
         </BrowserRouter>
     </React.StrictMode>,
